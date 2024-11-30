514bb07e7f0f7154dbb055a75aa2aace221dccbc
Kashif Raza <kashiraza102004@gmail.com>
2024-11-30 15:14:21 +0500

    removed un-needed imports

--- app/src/main/java/com/example/assignment4/Login_Activity.java
+++ app/src/main/java/com/example/assignment4/Login_Activity.java
@@ -2,17 +2,10 @@ package com.example.assignment4;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login_Activity extends AppCompatActivity {
